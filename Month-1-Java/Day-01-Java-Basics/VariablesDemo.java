cc0458c981ec5bc66ee417dcfe37ee6d3837962d
bhanuprakashbandam <bhanuprakashbandam@gmail.com>
2026-02-08 04:55:28 +0530

    Day 02: Variables and Data Types

---
+++ Month-1-Java/Day-01-Java-Basics/VariablesDemo.java
@@ -0,0 +1,16 @@
+class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 22;
+        double salary = 45000.75;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Bhanu";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
