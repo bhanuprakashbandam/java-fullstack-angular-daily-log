0d4dd02d39cfbcffe56c2b01b5e5d0972892f315
bhanuprakashbandam <bhanuprakashbandam@gmail.com>
2026-02-09 21:37:27 +0530

    Day 08: OOP basics – Class and Object

---
+++ Month-1-Java/Day-08-Class-Object/ClassObjectDemo .java
@@ -0,0 +1,14 @@
+class ClassObjectDemo  {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.name = "Bhanu";
+        s1.age = 22;
+        s1.display();
+
+        Student s2 = new Student();
+        s2.name = "Ravi";
+        s2.age = 21;
+        s2.display();
+    }
+}
